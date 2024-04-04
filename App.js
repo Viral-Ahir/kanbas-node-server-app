daa3441b740a0255c6c44d402d09dfc384ed9850
Viral-Ahir <ahirviral01@gmail.com>
2024-04-04 15:32:29 -0400

    First Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+Hello(app);
+AssignmentRoutes(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
---
+++ Kanbas/Database/index.js
@@ -0,0 +1,7 @@
+import courses from "./Courses.js";
+import modules from "./modules.js";
+import assignments from "./Assignments.js";
+import users from "./users.js";
+import grades from "./grades.js";
+import enrollments from "./enrollments.js";
+export default { courses, modules, assignments, users, grades, enrollments };
