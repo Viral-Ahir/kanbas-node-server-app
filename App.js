ad2a14462f84019f9c1089d1d846101f4de1d478
Viral-Ahir <ahirviral01@gmail.com>
2024-04-22 17:09:04 -0400

    JavaScript and Node.js Assignment 6 DB Updates

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import { mongoose } from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 const sessionOptions = {
